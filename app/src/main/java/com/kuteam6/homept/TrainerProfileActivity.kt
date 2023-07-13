9552a235fb0e18ef4de1c622f53f62e600e25325
정소민 <jeongsomin@jeongsomin-ui-MacBookAir.local>
2023-07-13 16:09:52 +0900

    feat: 회원가입 api 연결

--- app/src/main/java/com/kuteam6/homept/TrainerProfileActivity.kt
+++ app/src/main/java/com/kuteam6/homept/TrainerProfileActivity.kt
@@ -12,24 +12,25 @@ import com.kuteam6.homept.restservice.data.UserData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class TrainerProfileActivity : AppCompatActivity() {
     lateinit var binding: ActivityTrainerProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTrainerProfileBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_trainer_profile)
+        setContentView(binding.root)
 
-        binding.btnSubmitTraineeProfile.setOnClickListener {
+        binding.btnSubmitTrainerProfile.setOnClickListener {
 
             lifecycleScope.launch(Dispatchers.Main) { // 비동기 형태라 외부 쓰레드에서 실행해야함
                 var category = ""
-                category += binding.purpose1.isChecked.toString()
-                category += binding.purpose2.isChecked.toString()
-                category += binding.purpose3.isChecked.toString()
-                category += binding.purpose4.isChecked.toString()
-                category += binding.purpose5.isChecked.toString()
-                category += binding.purpose6.isChecked.toString()
+                if (binding.purpose1.isChecked) category += "1" else category += "0"
+                if (binding.purpose2.isChecked) category += "1" else category += "0"
+                if (binding.purpose3.isChecked) category += "1" else category += "0"
+                if (binding.purpose4.isChecked) category += "1" else category += "0"
+                if (binding.purpose5.isChecked) category += "1" else category += "0"
+                if (binding.purpose6.isChecked) category += "1" else category += "0"
 
                 var name = intent.getStringExtra("name")
                 var id = intent.getStringExtra("id")
@@ -37,15 +38,16 @@ class TrainerProfileActivity : AppCompatActivity() {
                 var gender = intent.getStringExtra("gender")
                 var birth = intent.getStringExtra("birth")
 
-
+                val pattern = DateTimeFormatter.ofPattern("yyyyMMdd")
+                val patterned = birth?.format(pattern)
+                val birthDate = LocalDate.parse(patterned, pattern)
 
                 var user: UserData = TrainerData(    // 트레이너 일경우 TrainerData생성해서 register함수 매개로
-                    name = name.toString(), id = id.toString(), password = pwd.toString(), gender = gender.toString(), birth = LocalDate.parse(birth),
+                    name = name.toString(), id = id.toString(), password = pwd.toString(), gender = gender.toString(), birth = birthDate,
                     isTrainee = false, userCategory = category, career = binding.careerHistory.text.toString(), lesson =  binding.lessionInfo.text.toString()
                 )
 
                 var userData: UserData? = ApiManager.register(user);
-                binding.tvRegiresult.setText(userData.toString());
             }
 
         }
