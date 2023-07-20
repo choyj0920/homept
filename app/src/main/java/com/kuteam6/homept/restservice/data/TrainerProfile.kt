680485b00d597014a571b839c2d31034c70c471f
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2023-07-21 05:42:40 +0900

    TrainerProfile class 추가

---
+++ app/src/main/java/com/kuteam6/homept/restservice/data/TrainerProfile.kt
@@ -0,0 +1,31 @@
+package com.kuteam6.homept.restservice.data
+
+import com.google.gson.annotations.SerializedName
+
+
+data class TrainerProfile(
+    @SerializedName("uid")
+    val uid: Int,
+    @SerializedName("trainer_id")
+    val trainer_id: Int,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("gender")
+    val gender: String,
+
+    @SerializedName("career")
+    val career: String,
+    @SerializedName("certificate")
+    val certificate: String?,
+    @SerializedName("lesson")
+    val lesson: String,
+    @SerializedName("usercategory")
+    val usercategory: String,
+    @SerializedName("location")
+    val location: String,
+){
+    override fun toString(): String {
+        return "${name} , 이력 : ${career}, 자격사항 ${certificate}, lesson : ${lesson}, category ${usercategory}, location : ${location}"
+    }
+}
+
