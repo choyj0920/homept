8627c2870c57142bd9537355eb00f8514594d065
정소민 <jeongsomin@jeongsomin-ui-MacBookAir.local>
2023-08-14 15:24:22 +0900

    feat: 로그인 시 사용자 정보 저장

--- app/src/main/java/com/kuteam6/homept/trainerRecommend/RecommendVPFragment.kt
+++ app/src/main/java/com/kuteam6/homept/trainerRecommend/RecommendVPFragment.kt
@@ -1,12 +1,14 @@
 package com.kuteam6.homept.trainerRecommend
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.kuteam6.homept.databinding.FragmentRecommendVpBinding
 import com.kuteam6.homept.restservice.data.TrainerProfile
+import com.kuteam6.homept.restservice.data.UserData
 
 class RecommendVPFragment(private val itemList : ArrayList<TrainerProfile>) : Fragment() {
 
@@ -15,6 +17,10 @@ class RecommendVPFragment(private val itemList : ArrayList<TrainerProfile>) : Fr
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        Log.d("isTrainee", UserData.userdata?.isTrainee.toString())
+        if(!UserData.userdata?.isTrainee!!) {
+            binding.btnPt.visibility = View.GONE
+        }
     }
 
     override fun onCreateView(
