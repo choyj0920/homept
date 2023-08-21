2d47495753e0ff45245715b5de17542be2f6fee4
정소민 <yjk70702002@gmail.com>
2023-08-21 20:52:30 +0900

    feat: sns 홈화면 구현

--- app/src/main/java/com/kuteam6/homept/sns/SnsAdapter.kt
+++ app/src/main/java/com/kuteam6/homept/sns/SnsAdapter.kt
@@ -1,4 +1,42 @@
 package com.kuteam6.homept.sns
 
-class SnsAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.kuteam6.homept.databinding.ItemSnsBinding
+import com.kuteam6.homept.restservice.data.Postdata
+
+class SnsAdapter(private val itemList : ArrayList<Postdata>) : RecyclerView.Adapter<SnsAdapter.ViewHolder>() {
+    private lateinit var itemClickListener : OnItemClickListener
+
+    interface OnItemClickListener{
+        fun onItemClick(postdata: Postdata)
+    }
+
+    fun setOnItemClickListener(onItemClickListener : OnItemClickListener) {
+        itemClickListener = onItemClickListener
+    }
+
+    inner class ViewHolder(val binding : ItemSnsBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(postdata: Postdata) {
+            // binding.snsProfileIv.setImageResource()
+            binding.snsNameTv.text = postdata.name
+            binding.snsTitleTv.text = postdata.title
+            binding.snsContentTv.text = postdata.content
+            binding.snsTypeTv.text = postdata.postcategory
+            binding.snsUploadDateTv.text = postdata.create_at
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : ViewHolder {
+        val binding = ItemSnsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = itemList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(itemList[position])
+    }
+
 }
--- app/src/main/java/com/kuteam6/homept/sns/SnsFragment.kt
+++ app/src/main/java/com/kuteam6/homept/sns/SnsFragment.kt
@@ -1,29 +1,48 @@
 package com.kuteam6.homept.sns
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kuteam6.homept.CategoryDialog
 import com.kuteam6.homept.R
 import com.kuteam6.homept.databinding.FragmentRecommendBinding
 import com.kuteam6.homept.databinding.FragmentSnsBinding
+import com.kuteam6.homept.restservice.ApiManager
+import com.kuteam6.homept.restservice.data.Postdata
+import com.kuteam6.homept.restservice.data.TrainerProfile
+import com.kuteam6.homept.tainerProfile.TrainersProfileActivity
+import com.kuteam6.homept.trainerSearch.SearchAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class SnsFragment : Fragment() {
     lateinit var binding: FragmentSnsBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    var snsAdapter: SnsAdapter? = null
+    var postDataList = ArrayList<Postdata>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSnsBinding.inflate(inflater, container, false)
+
+        lifecycleScope.launch(Dispatchers.Main) {
+            var resultList = ApiManager.getPost(uid = null)
+            Log.d("post", resultList?.get(0)!!.name)
+            if(resultList!=null) {
+                postDataList = resultList.toTypedArray().toCollection(ArrayList<Postdata>())
+                snsAdapter = SnsAdapter(postDataList)
+                binding.snsRv.adapter = snsAdapter
+                binding.snsRv.layoutManager = LinearLayoutManager(context)
+            }
+        }
 
         return binding.root
     }
